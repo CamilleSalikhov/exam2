13baf7cc2441c18dfe2d9e400fb89cb392036a2b
your_username <dontlookbackinanger@yandex.ru>
2023-03-23 20:14:45 +0300

    first

---
+++ .access.php
@@ -0,0 +1,12 @@
+<?
+$PERM["rew"]["6"]="W";
+$PERM["news"]["G5"]="W";
+$PERM["news"]["G2"]="R";
+$PERM["index.php"]["G5"]="R";
+$PERM["search"]["5"]="W";
+$PERM["products"]["5"]="W";
+$PERM["services"]["5"]="W";
+$PERM["contacts"]["5"]="W";
+$PERM["company"]["5"]="W";
+$PERM["/"]["*"]="R";
+?>
---
+++ export sql.php
@@ -0,0 +1,15 @@
+FVkQfGYUBgUtCUVcBBcBCgsTAQ==
+
+Мы делаем во втором билете:
+4.[ex1-7] Внедрить цепочкунавигации
+
+
+7.
+$myImageFirst = CFile::ResizeImageGet($arItem["PREVIEW_PICTURE"]["ID"], array("width"=> 56, "height"=>64), BX_RESIZE_IMAGE_PROPORTIONAL, true);
+	 
+
+8.
+
+Мы делаем в третьем билете:
+
+[ex1-2]
